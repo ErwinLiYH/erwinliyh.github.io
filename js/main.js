36ef97eba6059736efcc6c349fce1078fb574850
Yuanhao Li <erwinli@qq.com>
2024-04-03 00:09:01 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
-"use strict";$(document).ready(function(){var e,a=$("body").overlayScrollbars({className:(window.backgroundDark||window.backgroundImageDark?"y"===localStore.getItem("hugo-theme-dream-is-dark"):window.darkNav)?"os-theme-light":"os-theme-dark",scrollbars:{autoHide:"scroll",clickScrolling:!0}}).overlayScrollbars();window.fixedNav&&(e=$(".dream-nav"),a.options("callbacks.onScroll",function(){var a=this.scroll().position.y,o=$(".fake-dream-nav");0<a?(e.addClass("fixed").css("background","y"===window.isDark?window.backgroundDark:window.background),$(".dream-single-aside").css("top",54),o.length||$('<div class="fake-dream-nav" />').css("height",54).insertBefore(e)):(e.removeClass("fixed").css("background","unset"),$(".dream-single-aside").css("top",0),o.remove())})),window.overlayScrollbarsInstance=a,$(".dream-flip-toggle").click(function(){$(".flip-container").toggleClass("flip-it")})});
+$(document).ready(() => {
+    let isDark = localStore.getItem('hugo-theme-dream-is-dark')
+    isDark = isDark ? isDark : window.defaultDark ? 'y' : isDark
+  
+    function setClassName() {
+      return (
+        window.backgroundDark || window.backgroundImageDark
+          ? localStore.getItem('hugo-theme-dream-is-dark') === 'y'
+          : window.darkNav
+      )
+        ? 'os-theme-light'
+        : 'os-theme-dark'
+    }
+  
+    const osInstance = $('body')
+      .overlayScrollbars({
+        className: setClassName(),
+        scrollbars: {
+          autoHide: 'scroll',
+          clickScrolling: true,
+        },
+      })
+      .overlayScrollbars()
+  
+    if (window.fixedNav) {
+      const nav = $('.dream-nav')
+      const fake = $('.fake-dream-nav')
+        nav.addClass('fixed').css('background', isDark === 'y' ? window.backgroundDark : window.background)
+        $('.dream-single-aside').css('top', 54)
+        if (!fake.length) {
+          $('<div class="fake-dream-nav" />').css('height', 54).insertBefore(nav)
+        }
+    }
+  
+    window.overlayScrollbarsInstance = osInstance
+  
+    function initFilp() {
+      $('.dream-flip-toggle').click(() => {
+        $('.flip-container').toggleClass('flip-it')
+      })
+    }
+  
+    initFilp()
+  })
+  
--- js/theme.js
+++ js/theme.js
@@ -1,1 +1,176 @@
-"use strict";var dark="inverted",localStore=window.localStorage,isDark=(isDark=localStore.getItem("hugo-theme-dream-is-dark"))||(window.defaultDark?"y":isDark),darkBackground=function(){(window.backgroundDark||window.backgroundImageDark)&&$("body").toggleClass("default").toggleClass("dark")},dark404=function(){var a;(window.backgroundDark||window.backgroundImageDark)&&$(".dream-404-container").length&&($(".dream-404-container h1").toggleClass(dark),(a=$(".dream-404-container button")).toggleClass(dark),a.toggleClass("secondary"))},darkNavMenu=function(){(window.backgroundDark||window.backgroundImageDark)&&$(".dream-nav").toggleClass(dark);var a=window.overlayScrollbarsInstance;window.fixedNav&&a&&0<a.scroll().position.y&&$(".dream-nav").css("background","y"===window.isDark?window.backgroundDark:window.background)},darkHeaderElements=function(){var a,e;$(".dream-header").length&&(a=$(".dream-header .ui.header"),e=$(".dream-header .ui.list"),a.toggleClass(dark),e.toggleClass(dark))},darkCards=function(){$(".dream-card").toggleClass(dark)},darkSingle=function(){var a=$(".dream-single .ui.segment");a.length&&(a.toggleClass(dark),$(".dream-single h1.ui.header").toggleClass(dark),setThemeForUtterances(),"function"==typeof setHighlightTheme&&setHighlightTheme(),$(".toc").toggleClass(dark),$(".actions").toggleClass(dark)),$(".dream-scroll-to-top").toggleClass(dark)},darkTables=function(){$(".dream-single table").map(function(){this.style.color?this.style.color="":this.style.color="black"})},darkPostsSection=function(){$(".ui.segment.dream-posts-section").toggleClass(dark)},darkCategoriesSection=function(){$(".ui.segment.dream-categories-section").toggleClass(dark)},darkTagsSection=function(){$(".ui.segment.dream-tags-section").toggleClass(dark)},darkBack=function(){$(".dream-back .ui.segment").toggleClass(dark)},darkFooter=function(){$("footer.ui.segment").toggleClass(dark)},darkSearch=function(){$("#dream-search").toggleClass(dark)};function toggleDark(){darkBackground(),dark404(),darkNavMenu(),darkHeaderElements(),darkCards(),darkSingle(),darkTables(),darkPostsSection(),darkCategoriesSection(),darkTagsSection(),darkBack(),darkFooter(),darkSearch(),Array.isArray(window.darkFunctions)&&darkFunctions.forEach(function(a){return a()})}var setThemeForUtterances=function(){var a=document.querySelector("iframe.utterances-frame");a&&a.contentWindow.postMessage({type:"set-theme",theme:"y"===isDark?"github-dark":"github-light"},"https://utteranc.es")};window.addEventListener("message",function(a){"https://utteranc.es"===a.origin&&setThemeForUtterances()});var iconSwitchs=$(".theme-switch");function themeSwitch(a){a.preventDefault(),isDark="y"===isDark?(iconSwitchs.removeClass("moon"),iconSwitchs.addClass("sun"),localStore.setItem("hugo-theme-dream-is-dark","n"),"n"):(iconSwitchs.removeClass("sun"),iconSwitchs.addClass("moon"),localStore.setItem("hugo-theme-dream-is-dark","y"),"y"),toggleDark()}"y"===isDark?(iconSwitchs.addClass("moon"),toggleDark()):iconSwitchs.addClass("sun");
+const dark = 'inverted'
+const localStore = window.localStorage
+let isDark = localStore.getItem('hugo-theme-dream-is-dark')
+isDark = isDark ? isDark : window.defaultDark ? 'y' : isDark
+
+const darkBackground = () => {
+  if (window.backgroundDark || window.backgroundImageDark) {
+    $('body').toggleClass('default').toggleClass('dark')
+  }
+}
+
+const dark404 = () => {
+  if (window.backgroundDark || window.backgroundImageDark) {
+    const dream404 = $('.dream-404-container')
+
+    if (dream404.length) {
+      $('.dream-404-container h1').toggleClass(dark)
+
+      const button = $('.dream-404-container button')
+      button.toggleClass(dark)
+      button.toggleClass('secondary')
+    }
+  }
+}
+
+const darkNavMenu = () => {
+  if (window.backgroundDark || window.backgroundImageDark) {
+    $('.dream-nav').toggleClass(dark)
+  }
+
+  const osInstance = window.overlayScrollbarsInstance
+  if (window.fixedNav && osInstance) {
+    $('.dream-nav').css('background', isDark === 'y' ? window.backgroundDark : window.background)
+  }
+}
+
+const darkHeaderElements = () => {
+  const header = $('.dream-header')
+
+  if (header.length) {
+    const title = $('.dream-header .ui.header')
+    const iconList = $('.dream-header .ui.list')
+
+    title.toggleClass(dark)
+    iconList.toggleClass(dark)
+  }
+}
+
+const darkCards = () => {
+  $('.dream-card').toggleClass(dark)
+}
+
+const darkSingle = () => {
+  const segments = $('.dream-single .ui.segment')
+
+  if (segments.length) {
+    segments.toggleClass(dark)
+
+    const title = $('.dream-single h1.ui.header')
+    title.toggleClass(dark)
+
+    setThemeForUtterances()
+    if (typeof setHighlightTheme === 'function') {
+      setHighlightTheme()
+    }
+
+    $('.toc').toggleClass(dark)
+    $('.actions').toggleClass(dark)
+  }
+
+  $('.dream-scroll-to-top').toggleClass(dark)
+}
+
+const darkTables = () => {
+  $('.dream-single table').map(function () {
+    if (this.style.color) {
+      this.style.color = ''
+    } else {
+      this.style.color = 'black'
+    }
+  })
+}
+
+const darkPostsSection = () => {
+  $('.ui.segment.dream-posts-section').toggleClass(dark)
+}
+
+const darkCategoriesSection = () => {
+  $('.ui.segment.dream-categories-section').toggleClass(dark)
+}
+
+const darkTagsSection = () => {
+  $('.ui.segment.dream-tags-section').toggleClass(dark)
+}
+
+const darkBack = () => {
+  $('.dream-back .ui.segment').toggleClass(dark)
+}
+
+const darkFooter = () => {
+  $('footer.ui.segment').toggleClass(dark)
+}
+
+const darkSearch = () => {
+  $('#dream-search').toggleClass(dark)
+}
+
+function toggleDark() {
+  darkBackground()
+  dark404()
+  darkNavMenu()
+  darkHeaderElements()
+  darkCards()
+  darkSingle()
+  darkTables()
+  darkPostsSection()
+  darkCategoriesSection()
+  darkTagsSection()
+  darkBack()
+  darkFooter()
+  darkSearch()
+
+  if (Array.isArray(window.darkFunctions)) {
+    darkFunctions.forEach((d) => d())
+  }
+}
+
+const setThemeForUtterances = () => {
+  const utterances = document.querySelector('iframe.utterances-frame')
+
+  if (utterances) {
+    utterances.contentWindow.postMessage(
+      {
+        type: 'set-theme',
+        theme: isDark === 'y' ? 'github-dark' : 'github-light',
+      },
+      'https://utteranc.es'
+    )
+  }
+}
+
+window.addEventListener('message', (e) => {
+  if (e.origin !== 'https://utteranc.es') {
+    return
+  }
+
+  setThemeForUtterances()
+})
+
+const iconSwitchs = $('.theme-switch')
+
+// Apply theme when first entering
+if (isDark === 'y') {
+  iconSwitchs.addClass('moon')
+  toggleDark()
+} else {
+  iconSwitchs.addClass('sun')
+}
+
+function themeSwitch(e) {
+  e.preventDefault()
+
+  if (isDark === 'y') {
+    iconSwitchs.removeClass('moon')
+    iconSwitchs.addClass('sun')
+    localStore.setItem('hugo-theme-dream-is-dark', 'n')
+    isDark = 'n'
+  } else {
+    iconSwitchs.removeClass('sun')
+    iconSwitchs.addClass('moon')
+    localStore.setItem('hugo-theme-dream-is-dark', 'y')
+    isDark = 'y'
+  }
+
+  toggleDark()
+}
